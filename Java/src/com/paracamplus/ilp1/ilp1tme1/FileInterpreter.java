2dfaad544b3143766599d19b75b458691d03ac4f
zhengdao <316321454@qq.com>
2023-09-21 17:00:52 +0200

    FileInterpreter

---
+++ Java/src/com/paracamplus/ilp1/ilp1tme1/FileInterpreter.java
@@ -0,0 +1,22 @@
+package com.paracamplus.ilp1.ilp1tme1;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.paracamplus.ilp1.interpreter.interfaces.EvaluationException;
+import com.paracamplus.ilp1.interpreter.test.InterpreterTest;
+import com.paracamplus.ilp1.parser.ParseException;
+
+public class FileInterpreter extends InterpreterTest{
+	public FileInterpreter(File file) {
+        super(file);
+    }
+
+
+    public static void main (String[] args) throws ParseException, IOException, EvaluationException {
+         String fileName = args[0];
+         File file = new File(fileName);
+         FileInterpreter fileInterpreter = new FileInterpreter(file);
+         fileInterpreter.processFile();
+    }
+}
