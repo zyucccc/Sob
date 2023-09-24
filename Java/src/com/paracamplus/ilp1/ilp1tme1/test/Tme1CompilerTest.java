ce297535871f24cb04e576cc77fb76d5415c468f
mengxiao <limengxiao57@gmail.com>
2023-09-24 02:24:35 +0200

    change

--- Java/src/com/paracamplus/ilp1/ilp1tme1/test/Tme1CompilerTest.java
+++ Java/src/com/paracamplus/ilp1/ilp1tme1/test/Tme1CompilerTest.java
@@ -1,13 +1,14 @@
 package com.paracamplus.ilp1.ilp1tme1.test;
 
-import com.paracamplus.ilp1.interpreter.test.InterpreterRunner;
-import com.paracamplus.ilp1.interpreter.test.InterpreterTest;
+import com.paracamplus.ilp1.compiler.test.CompilerRunner;
+import com.paracamplus.ilp1.compiler.test.CompilerTest;
+
 import org.junit.runners.Parameterized;
 
 import java.io.File;
 import java.util.Collection;
 
-public class Tme1CompilerTest extends InterpreterTest {
+public class Tme1CompilerTest extends CompilerTest  {
     protected static String[] samplesDirName = { "SamplesTME1" };
     
     public Tme1CompilerTest(File file) {
@@ -16,6 +17,6 @@ public class Tme1CompilerTest extends InterpreterTest {
 
     @Parameterized.Parameters(name = "{0}")
     public static Collection<File[]> data() throws Exception {
-        return InterpreterRunner.getFileList(samplesDirName, pattern);
+        return CompilerRunner.getFileList(samplesDirName, pattern);
     }
 }
