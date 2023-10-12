c3138cae4bd6532e0ff1b45cba8a8a933319f9f1
zhengdao <316321454@qq.com>
2023-10-12 15:54:32 +0200

    compiler

--- Java/src/com/paracamplus/ilp1/ilp1tme3/C/sinus.c
+++ Java/src/com/paracamplus/ilp1/ilp1tme3/C/sinus.c
@@ -12,11 +12,11 @@ ILP_sinus (ILP_Object val)
 {
      if ( ILP_isInteger(val) ) {
 
-     return ILP_make_float(sin(val));
+     return ILP_make_float(sin(val->_content.asInteger));
 
      }else if ( ILP_isFloat(val) ) {
 
-    	 return ILP_make_float(sin(val));
+    	 return ILP_make_float(sin(val->_content.asFloat));
     }
      return  ILP_domain_error("une valeur non numérique", val);
 }
